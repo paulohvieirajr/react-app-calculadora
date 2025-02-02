3562331abfa790d39339167c2d21fd01a472f7bd
Paulo Vieira <paulohvieirajr@hotmail.com>
2025-02-02 18:27:32 -0300

    Função de soma e igual

--- src/App.js
+++ src/App.js
@@ -4,19 +4,54 @@ import Button from './components/Button'
 import { Container, Content, Row, Column } from './styles'
 import { useState } from 'react'
 
-
-
 const App = () =>{
   const [currentNumber, setCurrentNumber] = useState(0);
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
 
   const handleOnClear = () => {
-    console.log('clear');
     setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
   }
 
   const handleAddNumber = (number) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`);
+  }
 
-    setCurrentNumber(prev => `${number === '0' ? '' : number}${prev === '0' ? '' : prev}`);
+  const handleSumNumbers = () => {
+    console.log(currentNumber);
+
+    if (firstNumber === '0') {      
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      setOperation('+');
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+      setOperation('');
+    }
+  }
+
+  const handleEquals = (num) => {
+    if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
+      switch (operation) {
+        case '+':
+          handleSumNumbers();
+          break;
+        case '-':
+          // handleSubtractNumbers();
+          break;
+        case '*':
+          // handleMultiplyNumbers();
+          break;
+        case '/':
+          // handleDivideNumbers();
+          break;
+        default:
+          break;
+      }
+    } 
   }
 
   return (
@@ -39,13 +74,13 @@ const App = () =>{
           <Button label="4" onClick={() => handleAddNumber('4')}/>
           <Button label="4" onClick={() => handleAddNumber('5')}/>
           <Button label="5" onClick={() => handleAddNumber('6')}/>
-          <Button label="+" onClick={() => handleAddNumber('')}/>
+          <Button label="+" onClick={() => handleSumNumbers()}/>
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')}/>
           <Button label="2" onClick={() => handleAddNumber('2')}/>
           <Button label="3" onClick={() => handleAddNumber('3')}/>
-          <Button label="=" onClick={() => handleAddNumber('')}/>
+          <Button label="=" onClick={() => handleEquals()}/>
         </Row>
       </Content>
     </Container>
